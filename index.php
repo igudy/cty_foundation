5aaf9d73d376f0881756a4a86b63d50dd80f1878
Goodness Igunma <Goodnessigunma1@gmail.com>
2022-02-13 23:48:25 +0100

    Added whatsapp number

--- index.php
+++ index.php
@@ -163,7 +163,7 @@
           <!-- <a href="https://web.facebook.com/igunma.goodness?_rdc=1&_rdr"><img src="images/facebook-icon.png"></a> -->
           <a href="https://www.instagram.com/ctyfoundation/"><img src="images/instagram-icon.png"></a>
           <a href="https://twitter.com/CTY_Foundation"><img src="images/twitter-icon.png"></a>
-          <a href="https://wa.me/2348x097967034"><img src="images/whatsapp-icon.png"></a>
+          <a href="https://wa.me/2348097967034"><img src="images/whatsapp-icon.png"></a>
           <!-- <a href="#"><img src="images/snapchat-icon.png"></a> -->
         </div>
       </div>
